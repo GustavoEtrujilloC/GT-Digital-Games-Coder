cd3a968ec4b59c200e34e01095ca0ac34013da89
Gustavo Trujillo <gustavoetrujilloc@gmail.com>
2024-05-10 14:06:54 +0200

    Update

--- src/assets/Components/Logo/Logo.jsx
+++ src/assets/Components/Logo/Logo.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Logo = () => {
   return (
     <>
-    <Link to="/"><img className="imgLogo" src="../src/assets/img/logo.png" alt="gt-digital-games"></img></Link>
+    <Link to="/"><img className="imgLogo" src="../public/logo.png" alt="gt-digital-games"></img></Link>
     </>
   )
 }
